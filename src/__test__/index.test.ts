83e35fc2dde9893f9483c37cb4004cba656467ca
dettalant <lilac@dettalant.com>
2019-07-22 09:52:24 +0900

    add isConvertHarfExclam setting test

--- src/__test__/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { addOne } from "#/index";
-
-describe("index", () => {
-  it("addOne(1) to return 2", () => {
-    expect(addOne(1)).toBe(2);
-  })
-})
---
+++ src/__test__/jandentOptions.test.ts
@@ -0,0 +1,27 @@
+import { Jandent } from "#/index";
+import { JandentArgs } from "#/interfaces";
+
+describe("jandentOptions", () => {
+
+  const defaultSettingJandent = new Jandent();
+
+  it("default jandent replaceStrings has harf exclamation key", () => {
+    const replaceStrings = defaultSettingJandent.chars.replaceStrings;
+    expect(replaceStrings["!"]).toBe("！");
+    expect(replaceStrings["?"]).toBe("？");
+  })
+
+  it("if disabled isConvertHarfExclam setting, replaceStrings haven't got harf exclamation key", () => {
+    const customInitArg: JandentArgs = ({
+      options: {
+        isConvertHarfExclam: false
+      }
+    } as JandentArgs);
+
+    const jandent = new Jandent(customInitArg);
+    const replaceStrings = jandent.chars.replaceStrings;
+
+    expect(replaceStrings["!"]).toBeUndefined();
+    expect(replaceStrings["?"]).toBeUndefined();
+  })
+})
