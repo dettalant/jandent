c7e96c58866212f223ab65858fdd55ff9768a9d2
dettalant <lilac@dettalant.com>
2019-08-02 10:08:14 +0900

    jestがなんかエラー吐くけどまた今度修正する

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,13 @@
 module.exports = {
   verbose: true,
   transform: {
+    ".*\\.(js)$": "jest",
     ".*\\.(ts)$": "ts-jest",
   },
   moduleFileExtensions: ["js", "ts"],
   moduleDirectories: ["node_modules"],
   moduleNameMapper: {
     "^#/(.+)": "<rootDir>/src/$1",
-    "^#packages/(.+)": "<rootDir>/packages/$1"
   },
-  modulePathIgnorePatterns: ["<rootDir>/packages"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts?)$"
 }
